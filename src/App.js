fac255ff831f9f8f10d14fec1fb4941a2d6e2dbb
Iryna B <imbigdash@gmail.com>
2023-02-03 10:51:49 +0000

    added link for git-page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
       <Weather />
       </header>
+      <p>Coded by <a href='https://iryna-bigdash.github.io/weathe-app-react/'>Iryna Bigdash</a></p>
     </div>
   );
 }
