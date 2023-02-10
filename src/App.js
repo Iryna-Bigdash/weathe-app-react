d718366ac8e8b7ca31dfe0679544b6a839017986
Iryna B <imbigdash@gmail.com>
2023-02-10 17:24:00 +0000

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import './App.css';
+import React from 'react';
 import Weather from './Weather';
 
 function App() {
   return (
     <div className="App">
+      <div className='container'>
+        <button className='btn primary-btn'>Hello</button>
+
       <header className="App-header">
       <Weather />
       </header>
       <p>Coded by <a href='https://iryna-bigdash.github.io/weathe-app-react/'>Iryna Bigdash</a></p>
+    </div>
+
+
     </div>
   );
 }
